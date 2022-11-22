6ed97e2dda7abbbd9b3318cdb4000b24920e018c
stephentheofanous <42064389+stephentheofanous@users.noreply.github.com>
2022-11-22 14:36:20 +0000

    contact form

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 $to      = 'stephen.theofanous@gmail.com';
 $from    = $_POST['email'];
 $email   = $_POST['email'];
-$subject = 'Received New Email!';
+$subject = 'Received New Piano Teaching Enquiry';
 $message = "Name: ".$_POST['name']."\nEmail: ".$_POST['email']."\nMessage: ".$_POST["message"]."";
 $headers = 'From: '.$from."\r\n" .
         'Reply-To: '.$email."\r\n" .
@@ -15,7 +15,7 @@ if(mail($to, $subject, $message, $headers))
 $to1      = $_POST['email'];
 $from1    = 'stephen.theofanous@gmail.com';
 $email1   = "stephen.theofanous@gmail.com";
-$subject1 = 'ST || Composition';
+$subject1 = 'ST || Piano Teacher';
 $message1 = "Thank you for getting in touch! I will respond to your message shortly.\n\nName: ".$_POST['name']."\nEmail: ".$_POST['email']."\nMessage: ".$_POST["message"]."";
 $headers1 = 'From: '.$from1."\r\n" .
         'Reply-To: '.$email1."\r\n" .
